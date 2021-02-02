b51eacf9c58c833b1b928b6bfc84c060129aa0da
Ishtarita <purincesaneko@gmail.com>
2021-02-02 00:06:11 -0600

    creando repositorio

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/') ## crea rutas
+def home():
+	return render_template ('index.html')
+
+@app.route('/contacto', methods = ['POST'])
+def contacto():
+	nombre = request.form.get('nombre')
+	return render_template('contacto.html', nombre=nombre)
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
+	##corre el servidor
